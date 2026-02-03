1dc14c9b3e5637d5ef28bd090bcc40b42d8390f5
[dyad] <mswork@e.mail.de>
2026-02-04 00:33:19 +0100

    [dyad] Add component tagger loader - wrote 4 file(s), added @dyad-sh/nextjs-webpack-component-tagger package(s)

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,17 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  webpack: (config, { dev }) => {
+    if (process.env.NODE_ENV === "development") {
+      config.module.rules.push({
+        test: /\.(jsx|tsx)$/,
+        exclude: /node_modules/,
+        enforce: "pre",
+        use: "@dyad-sh/nextjs-webpack-component-tagger",
+      });
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
